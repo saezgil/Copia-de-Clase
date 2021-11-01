06a46f55292f91a2496d85a46324e1b62334a706
Martín Rodríguez <neomartinr@gmail.com>
2021-11-01 20:02:13 -0300

    loops while

---
+++ JS/loops/while.js
@@ -0,0 +1,16 @@
+let loops = 5;
+let i = 1;
+let total = 0;
+while(i <= loops) {
+    // document.write("Veces que se ejecuta el bucle:" + i + "<br>");
+    total = total + i;
+    i++;
+}
+
+console.log("Total: " + total);
+
+
+while(age == false) {
+    age = prompt("¿Cuál es tu edad?");
+    age = parseInt(age);
+}
